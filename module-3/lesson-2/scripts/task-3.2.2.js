63c21659b9c6f651d3edf8b8501dda04be1d8860
lawalea <77463958+lawalea@users.noreply.github.com>
2021-02-16 19:52:10 +0000

    javascript functions

---
+++ module-3/lesson-2/scripts/task-3.2.2.js
@@ -0,0 +1,14 @@
+function get_full_name (first_name, last_name){
+        let full_name = first_name + ' ' + last_name;
+        console.log(full_name);
+}
+
+var f_name ='Laura';
+var l_name ='Baines';
+
+var f_name2 ='Simon';
+var l_name2 ='Baines';
+
+
+get_full_name(f_name, l_name);
+get_full_name(f_name2, l_name2);
--- module-3/lesson-2/scripts/task-3.2.3.js
+++ module-3/lesson-2/scripts/task-3.2.3.js
@@ -1,14 +1,16 @@
-/* EXAMPLE */
-
-function greet(greeting, name) {
-  return greeting + ' ' + name;
+function get_full_name (first_name, last_name){
+  let full_name = first_name + ' ' + last_name;
+  return full_name;
 }
 
-var message = greet('Hello', 'Luke');
-    console.log( message ); // Output: Hello Luke
+var f_name ='Laura';
+var l_name ='Baines';
 
-/* EXAMPLE */
+var f_name2 ='Simon';
+var l_name2 ='Baines';
 
-// ADD YOUR CODE BELOW HERE
 
-// ADD YOUR CODE ABOVE HERE
+var full_name1 = get_full_name(f_name, l_name);
+console.log(full_name1);
+var full_name2 = get_full_name(f_name2, l_name2);
+console.log(full_name2);
