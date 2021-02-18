6a7e518bb29594df8b9b76d40cc82396168161de
lawalea <77463958+lawalea@users.noreply.github.com>
2021-02-18 19:30:22 +0000

    First javascript task - 3.3.1

---
+++ module-3/lesson-3/scripts/task-3.3.1.js
@@ -0,0 +1,22 @@
+function percentageCalculator(number,percent){
+    var blnValidated = true;
+    if(isNaN(number)){
+        blnValidated = false;
+    }
+    if(isNaN(percent)) {
+        blnValidated = false;
+    }
+    if(blnValidated){
+        var onePercent = number /100;
+        var percentValue = onePercent * percent;
+        return percentValue;
+    } else {
+        console.log ('both arguments need to be numbers');
+    }
+}
+
+var sum = percentageCalculator(100,50);
+console.log(sum);
+
+var sum = percentageCalculator(1000,50);
+console.log(sum);
