7c903ffca0ee6f773b40c8d8c8d54f078c733020
lawalea <77463958+lawalea@users.noreply.github.com>
2021-02-23 20:08:25 +0000

    Array in javascript

---
+++ module-3/lesson-4/scripts/task-3.4.2.js
@@ -0,0 +1,6 @@
+var arrFavouriteFoods = ['Lasagne', 'Fish and Chips', 'Pizza']
+
+for(var counter = 0; counter < arrFavouriteFoods. length; counter++){
+    let food = arrFavouriteFoods [counter];
+    console.log(counter + ': ' + food);
+}
